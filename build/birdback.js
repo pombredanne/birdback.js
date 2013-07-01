08fc2e0c1b56954865a148fbd6c31545881f728e
jean-philippe serafin <serafinjp@gmail.com>
2013-07-01 17:43:11 +0200

    Adds birdback dev email to birdback.js build

--- build/birdback.js
+++ build/birdback.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013 Birdback
+ * Copyright (c) 2013 Birdback <dev@birdback.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
